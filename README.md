a28afdcbcf83672548779444a2a79c9a3637a6dd
Andrew Clemons <andrew.clemons@gmail.com>
2017-06-20 09:15:22 +1200

    Add more logging from driver and selenium

--- README.md
+++ README.md
@@ -3,7 +3,10 @@ Testing chromedriver in headless mode using the selenium ruby-bindings.
 Opening a tab and switching to it seems to be currently broken.
 
         $ chromedriver --version
-        ChromeDriver 2.29
+        ChromeDriver 2.30.477691 (6ee44a7247c639c0703f291d320bdf05c1531b57)
 
         $ chromium --version
         Chromium 59.0.3071.86
+
+        $ google-chrome-beta --version
+        Google Chrome 60.0.3112.32 beta
--- run.rb
+++ run.rb
@@ -1,6 +1,9 @@
 require 'selenium-webdriver'
 require 'headless'
 
+Selenium::WebDriver.logger.level = :debug
+Selenium::WebDriver.logger.output = File.join(Dir.pwd, 'tmp', 'selenium.log')
+
 # test file which opens something in a new tab
 file = "file://#{File.join(Dir.pwd, 'test.html')}"
 
@@ -11,10 +14,9 @@ at_exit do
   @headless.destroy if @headless
 end
 
-# capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(chromeOptions: { args: [] })
-capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(chromeOptions: { args: [ '--headless' ] })
+capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(chrome_options: { args: %w(--headless --disable-gpu window-size=1200x800), binary: '/usr/bin/chromium' }) # /usr/bin/google-chrome-beta
 
-@driver = Selenium::WebDriver.for(:chrome, desired_capabilities: capabilities)
+@driver = Selenium::WebDriver.for(:chrome, desired_capabilities: capabilities, driver_opts: { args: %w(--verbose --log-path=tmp/chromedriver.log) })
 
 @driver.navigate.to(file)
 @driver.find_element(:link_text, 'New Tab').click
